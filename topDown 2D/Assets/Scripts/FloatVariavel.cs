54a823ad7cdefdfcfbdf86efac5d9c94aaa3a0e9
luisjti <71151598+luisjti@users.noreply.github.com>
2022-12-24 11:08:01 -0300

    Delete topDown 2D directory

--- topDown 2D/Assets/Scripts/FloatVariavel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Variavel/Float")]
-public class FloatVariavel : ScriptableObject
-{
-    public float valor;
-}
--- topDown 2D/Assets/Scripts/PowerUpEffect.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PowerUpEffect : ScriptableObject
-{
-    public abstract void Apply (GameObject alvo);
-    //Método abstrato para ser implementado no script de cada power up
-}
