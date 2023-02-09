c4f8cd69816d16236494a7b9414ed206f7cecfde
luisjti <luis_zaiden_assis@hotmail.com>
2023-02-09 20:40:50 -0300

    spawn e sprites poçoes

---
+++ TopDown Polido/Assets/Scripts/Gerais/SpawnPotion.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPotion : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objetosPrefab;
+    [SerializeField]
+    private GameObject[] posicoes;
+    [SerializeField]
+    private int numeroItensDaFase = 1;
+    [SerializeField]
+    private float spawnTime;
+    private int i;
+    private int randomPos, randomItem;
+
+    private void Start()
+    {
+        //Limita a quantidade de itens entre 1 e o número de locais posíveis para spawnar o item
+        numeroItensDaFase = Mathf.Clamp(numeroItensDaFase, 1, posicoes.Length); 
+        for (i=0; i < numeroItensDaFase; i++) 
+        { //Invoca a função de criação de itens aleatórios no início do game
+            Invoke("SpawnRandom", spawnTime); 
+        }
+
+    }
+
+    private void SpawnRandom()
+    {
+        randomItem = Random.Range(0, objetosPrefab.Length); //Sorteia qual o tipo de item
+        randomPos = Random.Range(0, posicoes.Length); //Sorteia qual o local para o item
+
+        //Cria um item com o tipo sorteado, em um local sorteado
+        Instantiate(objetosPrefab[randomItem], posicoes[randomPos].transform.position, transform.rotation); 
+    }
+}
